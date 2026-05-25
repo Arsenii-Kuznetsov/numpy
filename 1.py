edab4c3b1ff7be2fe249caeba691fe051e8168cc
Arsenii Kuznetsov <arskuz2007@mail.ru>
2026-05-25 18:16:03 +0300

    hgh

--- 1.py
+++ 1.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 try:
-    N = int(input())
+    N = int(input('Введите натуральное число: '))
 except ValueError:
     exit('Введено некорректное число')
 if N <= 0:
     exit('Чмсло должно быть положительным')
+print('Введены матрицы')
 A = np.random.randint(1, 101, size=(N, N))
 print(A)
 B = np.random.randint(1, 101, size=(N, N))
 print(B)
 C = np.array([A > B, A < B, A == B])
+print('Получена матрица')
 print(C)
