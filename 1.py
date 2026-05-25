c96788781fa3827fe759523ed099c9943de6e62e
Arsenii Kuznetsov <arskuz2007@mail.ru>
2026-05-25 18:11:49 +0300

    btybt

---
+++ 1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+try:
+    N = int(input())
+except ValueError:
+    exit('Введено некорректное число')
+if N <= 0:
+    exit('Чмсло должно быть положительным')
+A = np.random.randint(1, 101, size=(N, N))
+print(A)
+B = np.random.randint(1, 101, size=(N, N))
+print(B)
+C = np.array([A > B, A < B, A == B])
+print(C)
